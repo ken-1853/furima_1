b7cc5f3792c8a552d8bbe6539064903fd91bcf52
ken-1853 <kentaro.wada@di-v.co.jp>
2021-03-13 19:02:17 +0900

    価格JSの不具合修正

--- app/javascript/packs/price_calc.js
+++ app/javascript/packs/price_calc.js
@@ -1,20 +1,20 @@
 function price (){
-  // const path = location.pathname
-  // const editPath = path.includes("items") && path.includes("edit")
-  // if(path.includes("items/new") ||  editPath){
-    console.log("price_calc.js起動中")
-    const inputPrice = document.getElementById("item-price").value;
+  const path = location.pathname
+  const editPath = path.includes("items") && path.includes("edit")
+  if(path.includes("items/new") ||  editPath){
+    const inputPrice = document.getElementById("item-price");
     const addTax = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
     inputPrice.addEventListener("input", () => {
-      const tax =  Math.floor(inputPrice * 0.1);
+      const inputValue = document.getElementById("item-price").value;
+      const tax =  Math.floor(inputValue * 0.1);
       addTax.innerHTML = tax;
-      profit.innerHTML = inputPrice - tax;
+      profit.innerHTML = inputValue - tax;
       // Mathオブジェクトのfloor()メソッドは、小数点以下を切り捨てます。
       // 引数には数値を指定します。 文字列を指定すると NaN を返します
       // 正規表現でNaNを回避する
     });
-  // }
+  }
 }
 
 window.addEventListener("load",price);
